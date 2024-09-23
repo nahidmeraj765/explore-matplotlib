46a91e9e7277fc55ed265c722966633bcb92264a
NaHid Hasan Meraj <nahid.meraj765@gmail.com>
2024-09-23 12:39:07 +0600

    first commit

---
+++ matplotlib/1-introduction.py
@@ -0,0 +1,7 @@
+'''
+-Matplotlib is a plotting library for the Python programming language and its numerical mathematics extension NumPy.
+-Matplotlib is a python library used for Data Visualization.
+-Matplotlib is 2D and 3D plotting python library.
+'''
+
+import matplotlib.pyplot as plt 
